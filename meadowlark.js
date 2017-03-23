694c67b08e1ab50154b6026e62525fd388fe794d
JP <jprinz@conestogac.on.ca>
2017-03-23 16:18:06 -0400

    Chapter 5 work up to Crosspage testing

--- meadowlark.js
+++ meadowlark.js
@@ -17,13 +17,22 @@ app.set('ip', process.env.NODE_IP || 'localhost');
 app.use(express.static(__dirname + '/public'));
 
 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
+
 app.get('/', function(req, res){
     res.render('home');
 });
 
 
 app.get('/about', function(req, res){
-    res.render('about', { fortune: fortune.getFortune() });
+    res.render('about', { fortune: fortune.getFortune(),
+                        pageTestScript: '/qa/tests-about.js'
+                        });
 });
 
 // custom 404 page
