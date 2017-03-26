415ee67f53645d89c808980a9bd2e0208b6ed6d7
JP <jprinz@conestogac.on.ca>
2017-03-26 14:52:17 -0400

    Chapter 5 code complete; Added the Oregon-Coast page; Added a dayOfWeek function to home page; Added new function to test suite;

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,22 @@
 var express = require('express');
 
-var app = express();
-
-var fortune = require('./lib/fortune.js');
-
 // set up handlebars view engine
-var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main'
+});
+
+// Custom scripts
+var fortune = require('./lib/fortune.js');
+var dayOfWeek = require('./lib/dayOfWeek.js');
+
+
+
+var app = express();
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.disable('x-powered-by');
 
 app.set('port', process.env.PORT || 3000);
 app.set('ip', process.env.IP || 'localhost');
@@ -17,26 +24,38 @@ app.set('ip', process.env.IP || 'localhost');
 app.use(express.static(__dirname + '/public'));
 
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
     next();
 });
 
 
-app.get('/', function(req, res){
-    res.render('home');
-});
-
-
 // Health check for Openshift
-app.get('/health', function(req, res){
+app.get('/health', function (req, res) {
     res.status(200);
     res.render('health');
 });
 
+// Display header info
+app.get('/headers', function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) {
+        s += name + ': ' + req.headers[name] + '\n';
+    };
+    res.send(s);
+});
 
-app.get('/about', function(req, res){
+// ##Routes
+app.get('/', function (req, res) {
+    res.render('home', {
+        dayOfWeek: dayOfWeek.getDayOfWeek()
+    });
+});
+
+
+app.get('/about', function (req, res) {
     res.render('about', {
         fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
@@ -44,32 +63,37 @@ app.get('/about', function(req, res){
 });
 
 
-app.get('/tours/hood-river', function(req, res){
+app.get('/tours/hood-river', function (req, res) {
     res.render('tours/hood-river');
 });
 
 
-app.get('/tours/request-group-rate', function(req, res){
+app.get('/tours/oregon-coast', function (req, res) {
+    res.render('tours/oregon-coast');
+});
+
+
+app.get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
 });
 
 
 
-// custom 404 page
-app.use(function(req, res){
+// Custom 404 page
+app.use(function (req, res) {
     res.status(404);
     res.render('404');
 });
 
 
-//custom 500 page
-app.use(function(err, req, res, next){
+// Custom 500 page
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
 
-app.listen(app.get('port'), app.get('ip'), function(){
+app.listen(app.get('port'), app.get('ip'), function () {
     console.log('Express started on http://' + app.get('ip') + ':' + app.get('port') + '; press Ctrl-C to terminate.');
 });
